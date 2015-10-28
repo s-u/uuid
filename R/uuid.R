36d4d271dc098292154ad84a8c13ec734a471c06
Murat Tasan <murat.tasan@sympoz.com>
2015-10-28 16:52:06 -0600

    added R wrappers, docs, and bumped package version

--- R/uuid.R
+++ R/uuid.R
@@ -1,1 +1,15 @@
 UUIDgenerate <- function(use.time = NA) .Call(UUID_gen, use.time)
+
+##--------------------------------------------------------------------------------
+## Vectorized.
+## Returns a length(uuid) logical vector.
+## If is.na(uuid[i]), then UUIDvalidate(uuid)[i] == NA.
+##
+## Validation is done via libuuid's uuid_parse, and is thus much faster than using regexs.
+##
+## Does **not** validate _type_ of UUID (i.e. doesn't distingush between DCE, MD5, MAC access, etc. types).
+##--------------------------------------------------------------------------------
+UUIDvalidate <- function(uuid) {
+    stopifnot(is.character(uuid))
+    .Call(UUID_validate, uuid)
+}
